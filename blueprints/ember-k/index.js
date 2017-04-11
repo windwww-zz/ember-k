327a5a3e9d38816b26e2cf6dfbf55a4bca8b5ea1
windwww <windwww@gmail.com>
2017-04-11 12:57:00 +0000

    install

--- blueprints/ember-k/index.js
+++ blueprints/ember-k/index.js
@@ -13,18 +13,12 @@ module.exports = {
   afterInstall: function() {
     /*
     return RSVP.all([
-<<<<<<< HEAD
       this.addPackageToProject('ember-k'),
       this.addPackageToProject('kinto'),
       this.addAddonToProject('ember-browserify'),
-=======
-      //this.addPackageToProject('ember-k'),
-      //this.addPackageToProject('kinto'),
-      //this.addAddonToProject('ember-browserify'),
->>>>>>> e52da8398cb6decc7bd37d29c136b9fa40bbcbbf
       //this.addPackageToProject('babel-runtime'),
     ]);*/
-    console.log('0.0.37\n\n');
+    console.log('0.0.30\n\n');
     /*return this.addPackageToProject('kinto')/*.then(()=>{
       return this.addAddonToProject('ember-browserify');
     });*/
