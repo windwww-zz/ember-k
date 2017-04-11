2b7fae731614ca1f03f38bd35da5a75545c1df92
windwww <windwww@gmail.com>
2017-04-11 07:30:39 +0000

    ground is done

--- addon/index.js
+++ addon/index.js
@@ -1,2 +1,7 @@
 import Kinto from 'npm:kinto';
-export default {name: "ember-kinto-s", K: Kinto};
+import kAdapter from 'ember-k/adapters/k-adapter';
+export default {
+  name: "ember-kinto-s",
+  K: Kinto,
+  "k-adapter": kAdapter,
+};
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('index');
+  this.route('index', {path: "/"});
 });
 
 export default Router;
