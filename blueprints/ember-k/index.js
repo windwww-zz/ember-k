82411dee960415a7a2e7050e661cb28078a31942
windwww <windwww@gmail.com>
2017-04-12 07:26:59 +0000

    install

--- blueprints/ember-k/index.js
+++ blueprints/ember-k/index.js
@@ -22,7 +22,16 @@ module.exports = {
     /*return this.addPackageToProject('kinto')/*.then(()=>{
       return this.addAddonToProject('ember-browserify');
     });*/
-  return this.addAddonToProject('ember-browserify');
+    /*
+    var promise = new RSVP.Promise((resolve, reject) => {
+      this.addAddonToProject('ember-browserify').then(()=>{
+        this.addPackageToProject('kinto').then(()=>{
+          resolve();
+        });
+      });
+    });
+
+    return promise;*/
 
   },
 
